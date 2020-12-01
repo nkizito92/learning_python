2c12c236ec80fb471447ebcc177a2ec67f24b52c
nkizito92 <nkizito57@gmail.com>
2020-12-01 09:13:17 -0600

    learn how to use the for loop and built FizzBuzz

--- learning.py
+++ learning.py
@@ -4,9 +4,26 @@ print(random.randrange(10,50))
 
 name = "Kizito"
 len(name)
+# Created a FizzBuzz function in Python
+def fizzBuzz(num):
+    for x in range(1,num+1):
+        if x % 5 == 0 and x % 3 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else: 
+            print(x)
+
+fizzBuzz(30)
+
+
 
 arr = [1,5,8,9]
+obj ={ "name":"kizito", "age":4}
 type(arr)
+type(obj)
 def little_func(name):
     print(name + " is my name!")
 
